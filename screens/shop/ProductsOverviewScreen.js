b278cd97d332b2c08a8dead8eafe454e644adc4b
faisalhatila <hatilas1994@gmail.com>
2020-08-20 03:06:50 -0700

    cart icon added on product overview screen

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { StyleSheet, FlatList, Text } from "react-native";
+import { StyleSheet, FlatList, Text, Platform } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import ProductItem from "../../components/shop/ProductItem";
 import * as cartActions from "../../store/actions/cart";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../../components/UI/HeaderButton";
 const ProductsOverViewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
   const dispatch = useDispatch();
@@ -27,6 +29,21 @@ const ProductsOverViewScreen = (props) => {
       )}
     />
   );
+};
+
+ProductsOverViewScreen.navigationOptions = {
+  headerTitle: "All Products",
+  headerRight: () => {
+    return (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Cart"
+          iconName={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+          onPress={() => {}}
+        />
+      </HeaderButtons>
+    );
+  },
 };
 
 const styles = StyleSheet.create({});
