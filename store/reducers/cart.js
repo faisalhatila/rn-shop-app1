86ef2e8e28ec00e96281da2133117e3fdbb0e017
faisalhatila <hatilas1994@gmail.com>
2020-08-20 01:31:42 -0700

    case created

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,8 +1,17 @@
+import { ADD_TO_CART } from "../actions/cart";
+import { add } from "react-native-reanimated";
+
 const initialState = {
   items: {},
   totalAmount: 0,
 };
 
 export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.product;
+      const prodPrice = addedProduct.price;
+      const prodTitle = addedProduct.title;
+  }
   return state;
 };
