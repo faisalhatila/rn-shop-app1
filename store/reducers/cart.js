c93b92492214d7d559e5c77ff86db88dd60cc923
faisalhatila <hatilas1994@gmail.com>
2020-08-20 02:02:42 -0700

    add to cart reducer created

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,5 +1,6 @@
 import { ADD_TO_CART } from "../actions/cart";
 import { add } from "react-native-reanimated";
+import CartItem from "../../models/cart-item";
 
 const initialState = {
   items: {},
@@ -12,6 +13,26 @@ export default (state = initialState, action) => {
       const addedProduct = action.product;
       const prodPrice = addedProduct.price;
       const prodTitle = addedProduct.title;
+      let updatedOrNewCartItem;
+      if (state.items[addedProduct.id]) {
+        //already have the item in the cart
+        updatedOrNewCartItem = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          prodPrice,
+          prodTitle,
+          state.items[addedProduct.id].sum + prodPrice
+        );
+      } else {
+        updatedOrNewCartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+      }
+      return {
+        ...state,
+        items: {
+          ...state.items,
+          [addedProduct.id]: updatedOrNewCartItem,
+        },
+        totalAmount: state.totalAmount + prodPrice,
+      };
   }
   return state;
 };
