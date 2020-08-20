63bf7115d463eef8b811c6abea0b78d3c379a129
faisalhatila <hatilas1994@gmail.com>
2020-08-19 23:17:55 -0700

    product detail screen initialized

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  ScrollView,
+  View,
+  Text,
+  Image,
+  Button,
+  StyleSheet,
+} from "react-native";
 
 const ProductDetailScreen = (props) => {
   return (
     <View>
-      <Text></Text>
+      <Text>Product Detail Screen</Text>
     </View>
   );
 };
