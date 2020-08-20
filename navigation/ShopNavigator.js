27baea4c3f7aec15e88bd9e064eda27b43d38e7e
Faisal Hatila <hatilas1994@gmail.com>
2020-08-21 01:00:41 +0500

    drawer and order navigator creatd

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,30 +1,55 @@
 import { createStackNavigator, createAp } from "react-navigation-stack";
 import { createAppContainer } from "react-navigation";
+import { createDrawerNavigator } from "react-navigation-drawer";
 import ProductsOverViewScreen from "../screens/shop/ProductsOverviewScreen";
 import Colors from "../constants/Colors";
 import { Platform } from "react-native";
 import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 import CartScreen from "../screens/shop/CartScreen";
-const Productsnavigator = createStackNavigator(
+import OrdersScreen from "../screens/shop/OrdersScreen";
+const defaultNavOptions = {
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+  },
+  headerTitleStyle: {
+    fontFamily: "open-sans-bold",
+  },
+  headerBackTitleStyle: {
+    fontFamily: "open-sans",
+  },
+  headerTintColor: Platform.OS === "android" ? "#fff" : "",
+};
+
+const ProductsNavigator = createStackNavigator(
   {
     ProductsOverview: ProductsOverViewScreen,
     ProductDetail: ProductDetailScreen,
     Cart: CartScreen,
   },
   {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
-      },
-      headerTitleStyle: {
-        fontFamily: "open-sans-bold",
-      },
-      headerBackTitleStyle: {
-        fontFamily: "open-sans",
-      },
-      headerTintColor: Platform.OS === "android" ? "#fff" : "",
+    defaultNavigationOptions: defaultNavOptions,
+  }
+);
+
+const OrdersNavigator = createStackNavigator(
+  {
+    Orders: OrdersScreen,
+  },
+  {
+    defaultNavigationOptions: defaultNavOptions,
+  }
+);
+
+const ShopNavigator = createDrawerNavigator(
+  {
+    Products: ProductsNavigator,
+    Orders: OrdersNavigator,
+  },
+  {
+    contentOptions: {
+      activeTintColor: Colors.primaryColor,
     },
   }
 );
 
-export default createAppContainer(Productsnavigator);
+export default createAppContainer(ShopNavigator);
