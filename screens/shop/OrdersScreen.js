3e5a6727603e9d5afe27132bcf78419d2086d7ca
Faisal Hatila <hatilas1994@gmail.com>
2020-08-21 01:12:12 +0500

    successfully landed on order screen page via drawer

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { FlatList, StyleSheet, Text } from "react-native";
+import { FlatList, StyleSheet, Text, Platform } from "react-native";
 import { useSelector } from "react-redux";
 import Order from "../../models/order";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../../components/UI/HeaderButton";
 
 const OrdersScreen = (props) => {
-  const orders = useSelector((state) => state.orders.orders);
+  const orders = useSelector((state) => state.order.orders);
   return (
     <FlatList
       data={orders}
@@ -14,7 +16,22 @@ const OrdersScreen = (props) => {
 };
 
 const styles = StyleSheet.create({});
-OrdersScreen.navigationOptions = {
-  headerTitle = "Your Orders"
-}
+OrdersScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: "Your Orders",
+    headerLeft: () => {
+      return (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Menu"
+            iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+            onPress={() => {
+              navData.navigation.toggleDrawer();
+            }}
+          />
+        </HeaderButtons>
+      );
+    },
+  };
+};
 export default OrdersScreen;
