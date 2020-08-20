9b367f19160ed773b48cc671e0600a53601ac7d1
faisalhatila <hatilas1994@gmail.com>
2020-08-20 03:19:42 -0700

    cart screen created

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,14 +1,51 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
+import Colors from "../../constants/Colors";
 
 const CartScreen = (props) => {
+  const cartTotalAmount = useSelector((state) => state.cart.totalAmount);
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.screen}>
+      <View style={styles.summary}>
+        <Text style={styles.summaryText}>
+          Total : <Text style={styles.amount}>${cartTotalAmount}</Text>
+        </Text>
+        <Button title="Order Now" />
+      </View>
+      <View>Cart Items</View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    margin: 20,
+  },
+  summary: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 20,
+    padding: 10,
+    shadowColor: "#000",
+    shadowOpacity: 0.6,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+  },
+  summaryText: {
+    fontFamily: "open-sans-bold",
+    fontSize: 18,
+  },
+  amount: {
+    color: Colors.accentColor,
+  },
+});
 
 export default CartScreen;
