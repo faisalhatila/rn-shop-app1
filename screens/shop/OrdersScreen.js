8f941b292372555b2f04a95ee8792b8d77aac5e7
Faisal Hatila <hatilas1994@gmail.com>
2020-08-21 01:01:44 +0500

    set order title on order screen navigation options

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FlatList, StyleSheet, Text } from "react-native";
 import { useSelector } from "react-redux";
+import Order from "../../models/order";
 
 const OrdersScreen = (props) => {
   const orders = useSelector((state) => state.orders.orders);
@@ -13,5 +14,7 @@ const OrdersScreen = (props) => {
 };
 
 const styles = StyleSheet.create({});
-
+OrdersScreen.navigationOptions = {
+  headerTitle = "Your Orders"
+}
 export default OrdersScreen;
