9e5d1ae550197446995756ed673d0967fb03dafc
Faisal Hatila <hatilas1994@gmail.com>
2020-08-21 00:42:29 +0500

    export addOrder in action/orders.js

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,6 +1,6 @@
 export const ADD_ORDER = "ADD_ORDER";
 
-const addOrder = (cartItems, totalAmount) => {
+export const addOrder = (cartItems, totalAmount) => {
   return {
     type: ADD_ORDER,
     orderData: { items: cartItems, amount: totalAmount },
