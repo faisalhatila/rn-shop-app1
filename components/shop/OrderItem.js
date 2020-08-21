c7438831f53611f6012336e42b5fe14ba4857d72
faisalhatila <hatilas1994@gmail.com>
2020-08-20 23:41:18 -0700

    order item amount fixed

--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -7,7 +7,7 @@ const OrderItem = (props) => {
   return (
     <View style={styles.orderItem}>
       <View style={styles.summary}>
-        <Text style={styles.totalAmount}>$ {props.amount.toFixed(2)}</Text>
+        <Text style={styles.totalAmount}>${props.amount}</Text>
         <Text style={styles.date}>{props.date}</Text>
       </View>
       <Button color={Colors.primaryColor} title="Show Details" />
