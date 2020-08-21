716753d20f10a2bada5e00586f11a712a38e4cd1
faisalhatila <hatilas1994@gmail.com>
2020-08-21 01:01:44 -0700

    user product screen created

--- screens/user/UserProductScreen.js
+++ screens/user/UserProductScreen.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-
+import { StyleSheet, FlatList } from "react-native";
+import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
 const UserProductScreen = (props) => {
+  const userProducts = useSelector((state) => state.products.userProducts);
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <FlatList
+      data={userProducts}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
+    />
   );
 };
 
