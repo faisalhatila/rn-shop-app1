3ce3457c2f54e9bcee7b2d8059a95d29cadcde05
faisalhatila <hatilas1994@gmail.com>
2020-08-20 23:18:35 -0700

    order item component integerated in order screen

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -4,13 +4,19 @@ import { useSelector } from "react-redux";
 import Order from "../../models/order";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
 
 const OrdersScreen = (props) => {
   const orders = useSelector((state) => state.order.orders);
   return (
     <FlatList
       data={orders}
-      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+      renderItem={(itemData) => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.date}
+        />
+      )}
     />
   );
 };
