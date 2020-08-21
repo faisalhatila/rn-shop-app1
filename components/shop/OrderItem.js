cc0cc3cfe030163d3bcaec0d5e18d8587bc70593
faisalhatila <hatilas1994@gmail.com>
2020-08-20 23:11:19 -0700

    order item component created

---
+++ components/shop/OrderItem.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import { StyleSheet, View, Text, Button } from "react-native";
+import Colors from "../../constants/Colors";
+
+const OrderItem = (props) => {
+  return (
+    <View style={styles.orderItem}>
+      <View style={styles.summary}>
+        <Text style={styles.totalAmount}>$ {props.amount.toFixed(2)}</Text>
+        <Text style={styles.date}>{props.date}</Text>
+      </View>
+      <Button color={Colors.primaryColor} title="Show Details" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  orderItem: {
+    shadowColor: "#000",
+    shadowOpacity: 0.6,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    elevation: 5,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    margin: 20,
+    padding: 10,
+  },
+  summary: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+  },
+  totalAmount: {
+    fontFamily: "open-sans-bold",
+    fontSize: 16,
+  },
+  date: {
+    fontFamily: "open-sans",
+    fontSize: 16,
+    color: "#888",
+  },
+});
+
+export default OrderItem;
