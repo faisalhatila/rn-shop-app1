d3cc2e0518e84f57781347dd43fd7daff882516a
faisalhatila <hatilas1994@gmail.com>
2020-08-22 04:43:41 -0700

    price removed from update product action function param

--- store/actions/products.js
+++ store/actions/products.js
@@ -18,7 +18,7 @@ export const createProduct = (title, description, imageUrl, price) => {
   };
 };
 
-export const updateProduct = (id, title, description, imageUrl, price) => {
+export const updateProduct = (id, title, description, imageUrl) => {
   return {
     type: UPDATE_PRODUCT,
     pd: id,
